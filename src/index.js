032d7ef4b08e51048b610e705af16ab6fa7e438b
Yauheni Shatau <jack13only@gmail.com>
2021-08-25 13:08:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,123 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function expressionCalculator(str) {
+    if (str.length === 0) return 0
+   
+    let str1 = str.trim()
+    
+    let arr = []
+    if (str1.includes(' ')) arr = str1.split(' ')
+    else arr = str1.split('')
+    const arrBrackets = []
+    const arrCheck = []
+
+// Check brackets-------------------------
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '(' || arr[i] === ')') arrBrackets.push(arr[i])
+        if (arrBrackets[0] === ')') return false;
+    }
+
+    if (arrBrackets.length % 2 === 1) return false
+    
+    if (arrBrackets.length !== 0) { 
+     
+   	arrCheck.push(arrBrackets[0])
+
+    	for (let i = 1; i < arrBrackets.length; i++) {
+        
+        	if (arrCheck[arrCheck.length - 1] === arrBrackets[i]) arrCheck.push(arrBrackets[i])
+        	else arrCheck.pop()
+
+    	}
+
+	if (arrCheck.length !== 0) return false
+
+    }
+
+    
+    
+    // Calc---------------------------------
+    
+    function calc(a, op, b) {
+        if (op === '*') return (+a * +b)
+        if (op === '/') return (+a / +b)
+        if (op === '+') return (+a + +b)
+        if (op === '-') return (+a - +b)
+    }
+    
+    function megaCalc(arr1) {
+
+        for (let i = 0; i < arr1.length - 1; i++) {
+            
+            if (arr1[i] === '*' || arr1[i] ==='/') {
+                arr1[i + 1] = calc(arr1[i - 1], arr1[i], arr1[i + 1])
+                arr1[i - 1] = ''
+                arr1[i] = ''
+            }
+        }
+
+        let arrTemp = []
+
+        for (let i = 0; i < arr1.length; i++) {
+            if(arr1[i] !== '') arrTemp.push(arr1[i])
+        }
+
+        arr1 = arrTemp.slice();
+        arrTemp.length = 0;
+
+        for(let i = 0; i < arr1.length - 1; i++) {
+            
+            if (arr1[i] === '+' || arr1[i] ==='-') {
+                arr1[i + 1] = calc(arr1[i - 1], arr1[i], arr1[i + 1])
+                arr1[i - 1] = ''
+                arr1[i] = ''
+            }
+        }
+
+        for (let i = 0; i < arr1.length; i++) {
+            if(arr1[i] !== '') arrTemp.push(arr1[i])
+        }
+
+        arr1 = arrTemp.slice();
+        arrTemp.length = 0;
+
+        return arr1
+    }
+
+    // console.log(megaCalc(arr));
+
+        ///Brackets------------------------
+
+        function result(array) {
+            let a = -1;
+            let b = -1;
+            let arrTemp1 = []
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] === '(') a = i
+                if (array[i] === ')') {
+                    b = i
+                    for (let j = a + 1; j < i; j++) {
+                        arrTemp1.push(array[j])
+                    }
+                    // console.log(`gegaCalc(arrTemp1) = ${megaCalc(arrTemp1)}`);
+
+                    array.splice(a, i - a + 1, megaCalc(arrTemp1))
+                    arrTemp1 = []
+                    i = -1;
+                    a = -1
+                    b = -1
+                }
+            }
+
+            return megaCalc(array);
+
+        }
+
+    // console.log(result(arr)[0].toFixed(4));
+    
+    return +result(arr)[0].toFixed(4)
 }
 
 module.exports = {
